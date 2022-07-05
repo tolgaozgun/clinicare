d6430b525072d637c12cbecb4d19e2585b128177
tolgaozgun <tolgaozgunn@gmail.com>
2022-07-05 11:38:31 +0300

    Project overhaul, apps changed

--- base/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-# Create your views here.
-
-
-class IndexView(View):
-    def get(self, request):
-        return render(request, 'main/index.html')
